9312b35f0ea3dab0aab397acd40c6003d047f37a
andrew-wilkes <github@urgentclick.com>
2020-04-13 09:59:33 +0900

    Add scene switching and other scene files

--- game/Main.gd
+++ game/Main.gd
@@ -1,5 +1,16 @@
 extends CenterContainer
 
+func _on_Numbers_button_down():
+	get_tree().change_scene("res://Numbers.tscn")
 
-func _ready():
-	pass
+
+func _on_Gates_button_down():
+	get_tree().change_scene("res://Gates.tscn")
+
+
+func _on_Circuits_button_down():
+	get_tree().change_scene("res://Circuits.tscn")
+
+
+func _on_Projects_button_down():
+	get_tree().change_scene("res://Projects.tscn")
