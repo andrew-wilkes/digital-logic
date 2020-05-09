c534f8c480365aa4023e694be2db0d4c3f75a231
andrew-wilkes <github@urgentclick.com>
2020-05-09 09:09:19 +0900

    Add filter to label text editor

--- game/LabelDialog.gd
+++ game/LabelDialog.gd
@@ -3,8 +3,8 @@ extends WindowDialog
 signal updated(txt)
 
 func _on_Button_button_down():
-	var txt = $M/HBox/TextEdit.text
-	if txt == "":
+	var txt = $M/HBox/LineEdit.text.strip_edges()
+	if txt.empty():
 		txt = "?"
 	emit_signal("updated", txt)
 	hide()
