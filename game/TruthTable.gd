75d7314a6aadc6d033d44d8592f8244161b0e8ec
andrew-wilkes <github@urgentclick.com>
2020-05-11 19:57:33 +0900

    Add truth checking function

--- game/TruthTable.gd
+++ game/TruthTable.gd
@@ -4,11 +4,13 @@ var g1
 var g2
 var ni
 var no
+var values
 
 func _ready():
 	g1 = find_node("Grid")
 	g2 = find_node("Grid2")
 	var data = {
+		title = "Three-to-eight Decoder",
 		inputs = ["A2","A1","A0"],
 		outputs = ["Y0","Y1","Y2","Y3","Y4","Y5","Y6","Y7"],
 		values = [
@@ -22,7 +24,43 @@ func _ready():
 			[1,1,1,0,0,0,0,0,0,0,1]
 		]
 	}
+	values = data.values
 	populate(data)
+	var ok = check_truth([
+			[0,0,0,1,0,0,1,0,0,0,0],
+			[0,0,1,0,1,0,0,0,0,0,0],
+			[0,1,0,0,0,1,0,0,0,0,0],
+			[0,1,1,0,0,0,1,0,0,0,0],
+			[1,0,0,0,0,0,0,1,0,0,0],
+			[1,0,1,0,0,0,0,0,1,0,0],
+			[1,1,0,0,0,0,0,0,0,1,0],
+			[1,1,1,0,0,0,0,0,0,0,1]
+		])
+	print(ok)
+
+
+func check_truth(data):
+	var ok = true
+	var n = 0
+	for row in data:
+		for i in no:
+			var v1 = values[n][i + ni]
+			var v2 = row[i + ni]
+			if v1 is String:
+				set_output(n, i, v2)
+			elif v1 != v2:
+				set_output(n, i, v2, Color.red)
+				ok = false
+			else:
+				set_output(n, i, v2, Color.green)
+		n += 1
+	return ok
+
+
+func set_output(row, col, txt, color = Color.white):
+	var l = g2.get_child(row * no + col + no)
+	l.text = String(txt)
+	l.modulate = color
 
 
 func populate(data):
