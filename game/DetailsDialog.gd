721987111d66bd86aa2fc662a474f4f28c66af1d
andrew-wilkes <github@urgentclick.com>
2020-05-18 18:34:19 +0900

    Improve details dialog

--- game/DetailsDialog.gd
+++ game/DetailsDialog.gd
@@ -3,14 +3,14 @@ extends WindowDialog
 signal updated(title, description)
 
 func set_text(title, description):
-	$M/VBox/Title.text = title
-	$M/VBox/Description.txt = description
+	find_node("Title").text = title
+	find_node("Description").txt = description
 
 
 func _on_Button_button_down():
-	var title = $M/VBox/Title.text.strip_edges()
+	var title = find_node("Title").text.strip_edges()
 	if title.empty():
 		title = "Untitled"
-	var description = $M/VBox/Description.text.strip_edges()
+	var description = find_node("Description").text.strip_edges()
 	emit_signal("updated", title, description)
 	hide()
