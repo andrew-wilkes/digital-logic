5eb8fcac12502a37521d563559a042d9b9ec5f4a
andrew-wilkes <github@urgentclick.com>
2020-05-21 23:00:21 +0900

    Close label popup when enter key is pressed

--- game/LabelDialog.gd
+++ game/LabelDialog.gd
@@ -9,6 +9,14 @@ func set_text(txt):
 
 
 func _on_Button_button_down():
+	done()
+
+
+func _on_LineEdit_text_entered(_new_text):
+	done()
+
+
+func done():
 	var txt = $M/HBox/LineEdit.text.strip_edges()
 	emit_signal("updated", txt)
 	hide()
