2c8eeadb0128432fefe488e29acdde5be4a2375a
andrew-wilkes <github@urgentclick.com>
2020-05-27 10:33:11 +0900

    Tidy hint text

--- game/tt.gd
+++ game/tt.gd
@@ -17,7 +17,7 @@ var data = {
 			[1,1,1,0,0,0,0,0,0,0,1]
 		],
 		desc = "This is an example of a decoder. It takes a 3-bit binary input and decodes it to one of eight possible outputs.",
-		hint = "We can combine AND gates and NOT gates. For example: [i]Y5 = A2 * [u]A1[/u] * A0[/i]"
+		hint = "To decode the inputs, we can combine AND gates and NOT gates.\n\nFor example: [i]Y5 = A2 * [u]A1[/u] * A0[/i]"
 		
 	},
 	"fulladder":
@@ -36,6 +36,6 @@ var data = {
 			[1,1,1,1,1]
 		],
 		desc = "A full adder adds together a carry input, A, and B to give two outputs: sum and carry out.",
-		hint = "We have 3 bits to add together producing numbers in a range 0 to 3.\nIn binary: 00 to 11 with the least significant bit the sum, and the most significant bit the carry out.\nTo get the sum we can use exclusive OR: [i]A @ B @ Cin[/i]\nThe carry out is high if at least two of the inputs are high, so: [i]Cout = A * B + A * Cin + B * Cin[/i]"
+		hint = "We have 3 bits to add together producing numbers in a range 0 to 3.\n\nIn binary: 00 to 11 with the least significant bit the sum, and the most significant bit the carry out.\n\nTo get the sum we can use exclusive OR: [i]A @ B @ Cin[/i]\n\nThe carry out is high if at least two of the inputs are high, so:\n[i]Cout = A * B + A * Cin + B * Cin[/i]"
 	}
 }
