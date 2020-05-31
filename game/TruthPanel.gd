bb8187cd9c9cc6e579a2ef6ed5bb188ad3c7648a
andrew-wilkes <github@urgentclick.com>
2020-05-31 17:43:17 +0900

    Add truth testing

--- game/TruthPanel.gd
+++ game/TruthPanel.gd
@@ -2,7 +2,7 @@ extends AcceptDialog
 
 signal test_button_down
 
-enum { CHECKING_INPUTS, CHECKING_OUTPUTS, CHECKING_TRUTH, FAILED }
+enum { CHECKING_INPUTS, CHECKING_OUTPUTS, CHECKING_TRUTH, FAILED, PASSED }
 
 var state
 var circuit
@@ -13,6 +13,9 @@ var table
 var color
 var passed = false
 var msg = ""
+var ipmap = []
+var opmap = []
+var x = 0 # Don't care value
 
 func _ready():
 	var b = Button.new()
@@ -53,7 +56,7 @@ func _on_Timer_timeout():
 	match state:
 		CHECKING_INPUTS:
 			if passed:
-				table.g1.get_child(i).modulate = color
+				set_cell_color("g1", i)
 				i += 1
 				passed = false
 			if i == data.inputs.size():
@@ -62,20 +65,23 @@ func _on_Timer_timeout():
 			else:
 				# Look for named input in circuit
 				var s = data.inputs[i]
+				var pos = 0
 				for ip in circuit.ips:
 					if ip.get_label() == s:
+						ipmap.append(pos)
 						passed = true
-						color = table.g1.get_child(i).modulate
-						table.g1.get_child(i).modulate = g.COLOR_ACTIVE
+						color = get_cell_color("g1", i)
+						set_cell_color("g1", i, 0)
 						break
+					pos += 1
 				if !passed:
 					state = FAILED
-					table.g1.get_child(i).modulate = g.COLOR_HIGH
+					set_cell_color("g1", i, 1)
 					msg = "Missing input: %s" % s
 			$Timer.start()
 		CHECKING_OUTPUTS:
 			if passed:
-				table.g2.get_child(i).modulate = color
+				set_cell_color("g2", i)
 				i += 1
 				passed = false
 			if i == data.outputs.size():
@@ -84,19 +90,64 @@ func _on_Timer_timeout():
 			else:
 				# Look for named output in circuit
 				var s = data.outputs[i]
+				var pos = 0
 				for op in circuit.ops:
 					if op.get_label() == s:
+						opmap.append(pos)
 						passed = true
-						color = table.g2.get_child(i).modulate
-						table.g2.get_child(i).modulate = g.COLOR_ACTIVE
+						color = get_cell_color("g2", i)
+						set_cell_color("g2", i, 0)
 						break
+					pos += 1
 				if !passed:
 					state = FAILED
-					table.g2.get_child(i).modulate = g.COLOR_HIGH
+					set_cell_color("g2", i, 1)
 					msg = "Missing output: %s" % s
 			$Timer.start()
 		CHECKING_TRUTH:
-			pass
+			if passed:
+				i += 1
+			if i == data.values.size():
+				i = 0
+				state = PASSED
+			else:
+				# Get row of values
+				var io = data.values[i]
+				# Apply input values
+				var isize = data.inputs.size()
+				var osize = data.outputs.size()
+				for n in isize:
+					circuit.ips[ipmap[n]].state = io[n]
+					set_cell_color("g1", isize * i + n + isize, 0)
+				# Check output values
+				passed = true
+				for n in data.outputs.size():
+					if bool(io[n + isize]) == circuit.ops[opmap[n]].state:
+						set_cell_color("g2", osize * i + n + osize, 0)
+					else:
+						set_cell_color("g2", osize * i + n + osize, 1)
+						passed = false
+				if !passed:
+					state = FAILED
+					msg = "Incorrect logic!"
+			$Timer.start()
 		FAILED:
 			$Msg.dialog_text = msg
 			$Msg.popup_centered()
+		PASSED:
+			$Msg.window_title = "Passed!"
+			$Msg.dialog_text = ""
+			$Msg.popup_centered()
+
+func get_cell_color(grid, index):
+	return table[grid].get_child(index).modulate
+
+
+func set_cell_color(grid, index, c = -1):
+	var col = color
+	match c:
+		0:
+			col = g.COLOR_ACTIVE
+		1:
+			col = g.COLOR_HIGH
+	table[grid].get_child(index).modulate = col
