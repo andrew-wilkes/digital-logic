281218adaf2d38539b6013206b379935d4aaabb0
andrew-wilkes <github@urgentclick.com>
2020-06-01 22:33:04 +0900

    Improve notificaton popup

--- game/NotificationPopup.gd
+++ game/NotificationPopup.gd
@@ -1,26 +1,36 @@
 extends Control
 
-var state = 0
+export var TEST = false
+export var GUTTER_OFFSET = 40
 
-func notify(txt):
-	$PopupPanel/Label.text = txt
-	$PopupPanel.popup_centered()
-	$PopupPanel.set_as_minsize()
-	$PopupPanel.popup_centered() # Realign to center after resizing
-	$PopupPanel.rect_position.y = get_viewport_rect().size.y - 40
-	$Anim.play("Fade")
+var _state = 0
 
-# Check timer autostart property for testing
+func _ready():
+	if TEST:
+		$Timer.start()
+
+
 func _on_Timer_timeout():
-	match state:
-		0: 
+	match _state:
+		0:
+			$Timer.wait_time = 2
+		1: 
 			notify("Hello")
-		1:
-			notify("Hello World Hello World Hello World")
 		2:
+			notify("The quick brown fox jumps over the lazy dog.")
+		3:
 			notify("Bye")
-	state += 1
+	_state += 1
 
 
 func _on_Anim_animation_finished(_anim_name):
 	$PopupPanel.hide()
+
+
+func notify(_text):
+	$PopupPanel/Label.text = _text
+	$PopupPanel.popup_centered()
+	$PopupPanel.set_as_minsize()
+	$PopupPanel.popup_centered() # Realign to center after resizing
+	$PopupPanel.rect_position.y = get_viewport_rect().size.y - GUTTER_OFFSET
+	$Anim.play("Fade")
