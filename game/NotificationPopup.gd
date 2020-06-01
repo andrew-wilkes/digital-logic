2044a4b9af7fff4de29b4c869bb4bcb566029912
andrew-wilkes <github@urgentclick.com>
2020-06-01 13:24:44 +0900

    Place notification popup near bottom of screen

--- game/NotificationPopup.gd
+++ game/NotificationPopup.gd
@@ -7,17 +7,20 @@ func notify(txt):
 	$PopupPanel.popup_centered()
 	$PopupPanel.set_as_minsize()
 	$PopupPanel.popup_centered() # Realign to center after resizing
+	$PopupPanel.rect_position.y = get_viewport_rect().size.y - 40
 	$Anim.play("Fade")
 
-
+# Check timer autostart property for testing
 func _on_Timer_timeout():
 	match state:
 		0: 
 			notify("Hello")
 		1:
-			$PopupPanel.hide()
 			notify("Hello World Hello World Hello World")
 		2:
-			$PopupPanel.hide()
 			notify("Bye")
 	state += 1
+
+
+func _on_Anim_animation_finished(_anim_name):
+	$PopupPanel.hide()
