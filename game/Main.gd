f4b8bfc027bb4564393e0d3d78e02fb5c503a018
andrew-wilkes <github@urgentclick.com>
2020-11-05 15:10:22 +0900

    Detect screen size changes

--- game/Main.gd
+++ game/Main.gd
@@ -1,7 +1,10 @@
 extends CenterContainer
 
+var wsize
+
 func _ready():
 	get_tree().get_root().connect("size_changed", self, "size_changed")
+	size_changed()
 
 
 func _on_Numbers_button_down():
@@ -21,6 +24,12 @@ func _on_Projects_button_down():
 
 
 func size_changed():
-	print("New panel size: ", get_viewport_rect().size)
+	wsize = get_viewport_rect().size
+	$VBox/Info.text = "Panel size: %d %d" % [wsize.x, wsize.y]
+	
 	# iPhone 5 320 x 568
 	# iPhone 6/6S iPhone 6/6S
+
+
+func _on_Button_button_down():
+	OS.set_window_size(wsize * 0.8)
