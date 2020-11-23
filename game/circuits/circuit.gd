7e5dece1ebfb6e4b337e11f03d86cf5d33cd520a
andrew-wilkes <github@urgentclick.com>
2020-11-23 13:44:02 +0900

    Use exports for circuit data

--- game/circuits/circuit.gd
+++ game/circuits/circuit.gd
@@ -1,28 +1,9 @@
 extends Control
 
-# Input patterns
 # left to right digits equate with top to bottom pins
 # vout index obtained from LSB left evaluation of the vin value 0,0,1 = 4
-var vin = [
-	[0,0,0],
-	[1,0,0],
-	[0,1,0],
-	[0,0,1],
-	[1,1,0],
-	[1,0,1],
-	[0,1,1],
-	[1,1,1]
-]
+export(Array, Array, int) var vin
 
-var vout = [
-	[0,0],
-	[1,0],
-	[1,0],
-	[0,1],
-	[1,0],
-	[0,1],
-	[0,1],
-	[1,1]
-]
+export(Array, Array, int) var vout
 
 export(String, MULTILINE) var info = "" 
