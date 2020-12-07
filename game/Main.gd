b215553d2beb3e4fa2ac227a3b8468c441afc22b
andrew-wilkes <github@urgentclick.com>
2020-12-07 12:54:53 +0900

    Add animation to home scene

--- game/Main.gd
+++ game/Main.gd
@@ -1,11 +1,4 @@
 extends Control
 
-var wsize
-
 func _ready():
-	get_tree().get_root().connect("size_changed", self, "size_changed")
-	size_changed()
-
-func size_changed():
-	wsize = get_viewport_rect().size
-	$VBox/Info.text = "%d x %d" % [wsize.x, wsize.y]
+	$Anim.play("Intro")
