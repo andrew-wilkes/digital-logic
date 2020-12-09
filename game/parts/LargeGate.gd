65e4391fe9741334edc035a37372c3acad05eb30
andrew-wilkes <github@urgentclick.com>
2020-12-09 17:15:09 +0900

    Center Gates scene gate

--- game/parts/LargeGate.gd
+++ game/parts/LargeGate.gd
@@ -22,14 +22,14 @@ func _ready():
 func reset():
 	set_gate(4) # XOR gate
 	for i in 3:
-		get_child(gate).get_child(i).modulate = Color.white
+		$x.get_child(gate).get_child(i).modulate = Color.white
 
 
 func set_gate(idx: int):
 	gate = idx
 	for i in 6:
-		get_child(i).visible = i == idx
-	get_node("C/Label").text = "%s Gate" % names[idx]
+		$x.get_child(i).visible = i == idx
+	get_node("x/C/Label").text = "%s Gate" % names[idx]
 	set_result(2)
 
 
@@ -40,12 +40,12 @@ func set_inputs():
 	else:
 		vals = [randi() % 2, randi() % 2]
 	for i in vals.size():
-		get_child(gate).get_child(i + 1).modulate = color_from_val(vals[i])
+		$x.get_child(gate).get_child(i + 1).modulate = color_from_val(vals[i])
 	return vals
 
 
 func set_output(v):
-	get_child(gate).get_child(0).modulate = color_from_val(v)
+	$x.get_child(gate).get_child(0).modulate = color_from_val(v)
 
 
 func color_from_val(v):
@@ -54,7 +54,7 @@ func color_from_val(v):
 
 func set_result(r: int):
 	for i in 3:
-		get_node("C/Result").get_child(i).visible = r == i
+		get_node("x/C/Result").get_child(i).visible = r == i
 
 
 func _on_Timer_timeout():
