74ee9a8793d098c05c5199581e7b247d91e57555
andrew-wilkes <github@urgentclick.com>
2020-12-19 19:39:39 +0900

    Mods to fix minor issues

--- game/Main.gd
+++ game/Main.gd
@@ -1,4 +1,9 @@
 extends Control
 
 func _ready():
+	if OS.get_name() == "Windows":
+		var screen_size = OS.get_screen_size()
+		var window_size = OS.get_window_size()
+		OS.set_window_position(Vector2((screen_size.x - window_size.x) * 0.5, 0))
+		OS.set_window_size(Vector2(window_size.x, screen_size.y - 75))
 	$Anim.play("Intro")
